e0e87a2926fddb22e2a1744ae1777fc11a35aa71
Davisralves <davialves324@gmail.com>
2022-07-02 12:16:59 -0300

    test: corrigindo teste dos botões da galeria de produtos

--- wine_pages/components/ProductGalery/ProductGalery.test.js
+++ wine_pages/components/ProductGalery/ProductGalery.test.js
@@ -1,14 +1,14 @@
 import ProductGalery from "./";
-import { getAllByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import wine from "../../mocks/winesjson";
 describe("Test if product galery render as aspected", () => {
 	it("Buttons are render correctly", () => {
 		render(<ProductGalery data={{ items: wine }} />);
-		const [button1, button2] = getAllByRole("button");
+		const [button1, button2] = screen.getAllByRole("button", {
+			name: "Adicionar",
+		});
 		expect(button1).toBeInTheDocument();
 		expect(button2).toBeInTheDocument();
-        expect(button1.value).toEqual("Adicionar")
-        expect(button2.value).toEqual("Adicionar")
 	});
 });
