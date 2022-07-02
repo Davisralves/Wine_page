07fe2794178c1d40d9ad936286765b61bd3d4d6c
Davisralves <davialves324@gmail.com>
2022-07-02 16:17:33 -0300

    test: adicioando testes ao index

--- wine_pages/components/ProductGalery/ProductGalery.test.js
+++ wine_pages/components/ProductGalery/ProductGalery.test.js
@@ -1,10 +1,10 @@
 import ProductGalery from "./";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import wine from "../../mocks/winesjson";
 describe("Test if product galery render as aspected", () => {
 	it("Buttons are render correctly", () => {
-		render(<ProductGalery data={{ items: wine }} />);
+		render(<ProductGalery data={{ items: wine }} filter={[0, 9999]} />);
 		const [button1, button2] = screen.getAllByRole("button", {
 			name: "Adicionar",
 		});
@@ -12,10 +12,12 @@ describe("Test if product galery render as aspected", () => {
 		expect(button2).toBeInTheDocument();
 	});
 	it("Cards are render correctly", () => {
-		render(<ProductGalery data={{ items: wine }} />);
+		render(<ProductGalery data={{ items: wine }} filter={[0, 9999]} />);
 		const cardsAndButtons = screen.getAllByTestId("button-and-card");
 		expect(cardsAndButtons).toHaveLength(2);
 		expect(cardsAndButtons[0]).toBeInTheDocument();
 		expect(cardsAndButtons[1]).toBeInTheDocument();
 	});
+
+
 });
