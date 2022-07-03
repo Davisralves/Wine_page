5bb36943932c2d4dc773904d0653ba3cccfaa3ac
Davisralves <davialves324@gmail.com>
2022-07-03 11:11:08 -0300

    test: corrigindo teste do index

--- wine_pages/test/index.test.js
+++ wine_pages/test/index.test.js
@@ -1,10 +1,10 @@
 import Home from "../pages/index";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
+import wine from "../mocks/winesjson";
 describe("Test if index page work as aspected", () => {
 	it("Test if first page render as aspected", () => {
-		render(<Home />);
+		render(<Home data={{ items: wine }} />);
 		const redefineSearch = screen.getByRole("heading", {
 			level: 4,
 			name: "Redefine sua busca",
