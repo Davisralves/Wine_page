b4e446eb7ede47a15c61aaa7a8762595f909e8c5
Davisralves <davialves324@gmail.com>
2022-07-03 21:02:50 -0300

    test: adicionando mock do useRouter para testes funcionarem corretamente

--- wine_pages/components/ProductGalery/ProductGalery.test.js
+++ wine_pages/components/ProductGalery/ProductGalery.test.js
@@ -1,7 +1,23 @@
 import ProductGalery from "./";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import wine from "../../mocks/winesjson";
+
+// ref: https://github.com/vercel/next.js/discussions/23034
+// By aeksco on 20 Sep 2019
+jest.mock("next/router", () => ({
+	useRouter() {
+		return {
+			route: "/",
+			pathname: "",
+			query: { page: 1 },
+			asPath: "",
+		};
+	},
+}));
+
+jest.spyOn(require("next/router"), "useRouter");
+
 describe("Test if product galery render as aspected", () => {
 	it("Buttons are render correctly", () => {
 		render(<ProductGalery data={{ items: wine }} filter={[0, 9999]} />);
@@ -18,6 +34,4 @@ describe("Test if product galery render as aspected", () => {
 		expect(cardsAndButtons[0]).toBeInTheDocument();
 		expect(cardsAndButtons[1]).toBeInTheDocument();
 	});
-
-
 });
--- wine_pages/test/index.test.js
+++ wine_pages/test/index.test.js
@@ -2,6 +2,21 @@ import Home from "../pages/index";
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import wine from "../mocks/winesjson";
+
+// ref: https://github.com/vercel/next.js/discussions/23034
+// By aeksco on 20 Sep 2019
+jest.mock("next/router", () => ({
+	useRouter() {
+		return {
+			route: "/",
+			pathname: "",
+			query: { page: 1 },
+			asPath: "",
+		};
+	},
+}));
+
+jest.spyOn(require("next/router"), "useRouter");
 describe("Test if index page work as aspected", () => {
 	it("Test if first page render as aspected", () => {
 		render(<Home data={{ items: wine }} />);
